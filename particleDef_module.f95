f99e4c5feeddcfab73cac597661237301d36f3e1
Katiska <Ahti.Katiska@helsinki.fi>
2014-12-04 00:52:27 +0200

    TODO added

--- particleDef_module.f95
+++ particleDef_module.f95
@@ -1,7 +1,8 @@
 MODULE particleDef
   type particle
     INTEGER :: id
-    REAL :: m, q
-    REAL, DIMENSION(3) :: pos, vel
+    REAL :: m, q ! Mass in kg, charge in elemental charges
+    REAL, DIMENSION(3) :: pos, vel ! Position in meters, velocity in m/s
   end type particle
+  REAL :: e_to_coulomb = 1.602176565E-19
 END MODULE particleDef
